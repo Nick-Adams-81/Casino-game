12875450d4e63577bc01ad89b39cc8dec4a9b8f5
nick adams <thebigdeal916@icloud.com>
2020-02-15 11:46:13 -0800

    casino game v5

--- script.js
+++ script.js
@@ -3,9 +3,64 @@ var hit = "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
 var deck = "https://deckofcardsapi.com/api/deck/new/"
 var stand = "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
 var foaas = "https://www.foaas.com/text/plain/asshole/:from"
-var playerHand = $("#player")
-var dealerHand = $("#dealer")
-var cardValue = {'2C': 2,'2H': 2,'2D': 2,'2S': 2,'3C': 3,'3H': 3,'3D': 3,'3S': 3,'4C': 4,'4H': 4,'4D': 4,'4S': 4,'5C': 5,'5H': 5,'5C': 5,'5S': 5,'6C': 6,'6H': 6,'6D': 6,'6S': 6,'7C': 7,'7H': 7,'7D': 7,'7S': 7,'8C': 8,'8D': 8,'8H': 8,'8S': 8,'9C': 9,'9H': 9,'9D': 9,'9S': 9,'0C': 10,'0H': 10,'0D': 10,'0S': 10,'JC': 10,'JH': 10,'JD': 10,'JS': 10,'QC': 10,'QH': 10,'QC': 10,'QS': 10,'KC': 10,'KH': 10,'KD': 10,'KS': 10,'AC': [11,1],'AH': [11,1],'AC': [11,1],'AS': [11,1]}
+var cardValue = {
+                 '2C': 2,
+                 '2H': 2,
+                 '2D': 2,
+                 '2S': 2,
+                 '3C': 3,
+                 '3H': 3,
+                 '3D': 3,
+                 '3S': 3,
+                 '4C': 4,
+                 '4H': 4,
+                 '4D': 4,
+                 '4S': 4,
+                 '5C': 5,
+                 '5H': 5,
+                 '5C': 5,
+                 '5S': 5,
+                 '6C': 6,
+                 '6H': 6,
+                 '6D': 6,
+                 '6S': 6,
+                 '7C': 7,
+                 '7H': 7,
+                 '7D': 7,
+                 '7S': 7,
+                 '8C': 8,
+                 '8D': 8,
+                 '8H': 8,
+                 '8S': 8,
+                 '9C': 9,
+                 '9H': 9,
+                 '9D': 9,
+                 '9S': 9,
+                 '0C': 10,
+                 '0H': 10,
+                 '0D': 10,
+                 '0S': 10,
+                 'JC': 10,
+                 'JH': 10,
+                 'JD': 10,
+                 'JS': 10,
+                 'QC': 10,
+                 'QH': 10,
+                 'QC': 10,
+                 'QS': 10,
+                 'KC': 10,
+                 'KH': 10,
+                 'KD': 10,
+                 'KS': 10,
+                 'AC': [11,1],
+                 'AH': [11,1],
+                 'AC': [11,1],
+                 'AS': [11,1]
+                } 
+var dealerHand = 0
+var playerHand = 0
+
+
 
 
 
@@ -30,22 +85,41 @@ $("#deal").click(function(){
     console.log(response)
     var tRow = $("#dealer");
     var card = $("<img>").attr('src',response.cards[0].image)
+                         .animate({height: '50%',width: '20%'})
     var card2 = $("<img>").attr('src',response.cards[2].image)
+                          .animate({height: '50%',width: '20%'})
 
     tRow.append(card)
     tRow.append(card2)
+
+  
   
 
     //player hand
     var tRow2 = $("#player");
     var cards = $("<img>").attr('src',response.cards[1].image)
+                          .animate({height: '50%',width: '20%'})
     var cards2 = $("<img>").attr('src',response.cards[3].image)
+                           .animate({height: '50%',width: '20%'})
     tRow2.append(cards)
     tRow2.append(cards2)
    
-      
+    dealerHand = cardValue[response.cards[0].code] + 
+                 cardValue[response.cards[2].code]
+          
+
+    console.log(dealerHand)
+
+    playerHand = cardValue[response.cards[1].code] +
+                 cardValue[response.cards[3].code]
+
+              
+   
+    console.log(playerHand)  
   
   })
+    
+
 });
 
    //take a hit
@@ -57,7 +131,13 @@ $("#hit").click(function(){
     console.log(response)
     var tRow2 = $("#player")
     var cards = $("<img>").attr('src', response.cards[0].image)
-    tRow2.append(cards)
+                          .animate({height: '50%',width: '20%'})
+  playerHand = playerHand + cardValue[response.cards[0].code] 
+  
+  console.log(playerHand)
+
+  tRow2.append(cards)
+
     
   })
   
@@ -71,16 +151,23 @@ $("#stand").click(function(){
     }).then(function(response){
       var tRow = $("#dealer");
       var cards = $("<img>").attr('src', response.cards[0].image)
+                            .animate({height: '50%', width: '20%'})
+      dealerHand = dealerHand + cardValue[response.cards[0].code]
+
+      console.log(dealerHand)
+
       tRow.append(cards)
+      
 
     })
+    
   
 
   }
   
 });
 
-console.log(cardValue["0C"])
+console.log(cardValue['4C'] + cardValue['0H'])
 
 
 
