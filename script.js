ff137e4c92d5378eaea2485883dc26d46934614b
Tuan Donald Nguyen <nguyentuand85@gmail.com>
2020-02-18 22:23:31 -0800

    added card scores

--- script.js
+++ script.js
@@ -6,69 +6,73 @@ var playerHand;
 var dealerHand;
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  
 
-//Start Game
-$("#deal").click(function(){
-  
-  $.ajax({
-    url: deal,
-    method: "GET"
+  //Start Game
+  $("#deal").click(function () {
+    $.ajax({
+      url: deal,
+      method: "GET"
 
-    //dealer hand
-  }).then(function(response){
-    console.log(response)
-    var tRow = $("#dealer");
-    var card = $("<img>").attr('src',response.cards[0].image)
-    var card2 = $("<img>").attr('src',response.cards[2].image)
+      //dealer hand
+    }).then(function (response) {
+      console.log(response)
+      var tRow = $("#dealer");
+      var card = $("<img>").attr('src', response.cards[0].image)
+        .animate({ height: '50%', width: '20%' }); $("<img>").attr('src', response.cards[0].image)
+          .animate({ height: '50%', width: '20%' })
+      //var card2 = $("<img>").attr('src', response.cards[2].image)
+      //.animate({ height: '50%', width: '20%' })
+      tRow.append(card)
+      //tRow.append(card2)
 
-    tRow.append(card)
-    tRow.append(card2)
+      //player hand
+      var tRow2 = $("#player");
+      var cards = $("<img>").attr('src', response.cards[1].image)
+        .animate({ height: '50%', width: '20%' })
+      var cards2 = $("<img>").attr('src', response.cards[3].image)
+        .animate({ height: '50%', width: '20%' })
+      tRow2.append(cards)
+      tRow2.append(cards2)
+      dealerHand = cardValue[response.cards[0].code] +
+        cardValue[response.cards[2].code]
+      console.log("dealer's hand count: " + dealerHand)
 
-    //player hand
-    var tRow2 = $("#player");
-    var cards = $("<img>").attr('src',response.cards[1].image)
-    var cards2 = $("<img>").attr('src',response.cards[3].image)
-    tRow2.append(cards)
-    tRow2.append(cards2)
-   
-      
-  
-  })
-});
+      //Update Dealer's card number
+      var dealerNum = $('#dealers')
+      dealerNum.text(dealerHand)
 
-   //take a hit
-$("#hit").click(function(){
-  $.ajax({
-    url: hit,
-    method: "GET"
-  }).then(function(response){
-    console.log(response)
-    var tRow2 = $("#player")
-    var cards = $("<img>").attr('src', response.cards[0].image)
-    tRow2.append(cards)
-  })
-});
+      playerHand = cardValue[response.cards[1].code] +
+        cardValue[response.cards[3].code]
+      console.log("players hand count: " + playerHand)
+
+      //Update player's card number
+      var playersNum = $('#players')
+      playersNum.text(playerHand)
+    })
+  });
+
+  //take a hit
+  $("#hit").click(function () {
+    $.ajax({
+      url: hit,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response)
+      var tRow2 = $("#player")
+      var cards = $("<img>").attr('src', response.cards[0].image)
+      tRow2.append(cards)
+    })
+  });
+
   //stand function
-$("#stand").click(function(){
-  
-});
+  $("#stand").click(function () {
 
-
-
-
-
-
-
-
-
-
-
+  });
 })
 
-  
+
 
 
 
